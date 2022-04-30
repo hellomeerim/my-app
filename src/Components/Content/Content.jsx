078bdafc487dcbf164a34963659c9b4bd7aefaf5
hellomeerim <eg.meerim@gmail.com>
2022-04-30 12:26:53 +0600

    feat

---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideBar from '../SideBar/SideBar';
+
+const Content = () => {
+    return (
+        <div className="">
+            <p>lnldndcfdvddf</p>
+        </div>
+    );
+};
+
+export default Content;
